04339a16550d7c35cac5efca07e4293f4f492e5d
Almeida-Oco <intjalmeida@gmail.com>
2017-11-26 16:54:37 +0000

    rand changes

--- LinearAnimation.js
+++ LinearAnimation.js
@@ -8,25 +8,6 @@ var DEGREE_TO_RAD = Math.PI / 180;
  */
 class LinearAnimation extends Animation {
 	constructor(speed, args) {
-		function calcAngle(vec1, vec2, distance) {
-			let c_angle = (vec2[0] - vec1[0]) / distance,
-					s_angle = (vec2[2] - vec2[2]) / distance,
-					ret = Math.acos(c_angle);
-
-			if (0 == c_angle){
-				if (s_angle == -1) return Math.PI /2;
-				else return 3 * Math.PI / 2;
-			}
-			else if (0 == s_angle) {
-				if (c_angle == 0) return 0;
-				else return Math.PI;
-			}
-			else if (c_angle > 0 && s_angle > 0) return ret;
-			else if (c_angle > 0 && s_angle < 0) return (Math.PI * 3/2) + ret;
-			else if (c_angle < 0 && s_angle > 0) return (Math.PI / 2) + ret;
-			else if (c_angle < 0 && s_angle < 0) return Math.PI + ret;
-		}
-
 		super(speed, args);
     this.last_pt = args[args.length-1];
 		this.pts = args;
@@ -34,16 +15,15 @@ class LinearAnimation extends Animation {
     this.angles = [];
     this.angles.push(0);
     for (let i = 1; i < (this.pts.length-1); i++) {
-      let vec1 = [this.pts[i][0] - this.pts[i-1][0], 0, this.pts[i][2] - this.pts[i-1][2]],
-          vec2 = [this.pts[i+1][0] - this.pts[i][0], 0, this.pts[i+1][2] - this.pts[i][2]];
+      let vec1 = [this.pts[i][0] - this.pts[i-1][0], 0, this.pts[i][2] - this.pts[i-1][2]], //first segment vector
+          vec2 = [this.pts[i+1][0] - this.pts[i][0], 0, this.pts[i+1][2] - this.pts[i][2]]; //second segment vector
 
 			super.normalizeVector(vec1); super.normalizeVector(vec2);
+
 			let dt_prod  = super.dotProduct(vec2, vec1),
 					cross = super.crossProduct(vec2, vec1),
-					angle = Math.acos(dt_prod);
+					angle = Math.atan2( super.dotProduct(cross, [0,1,0]), dt_prod);
 
-			// if (cross[1] < 0 && i <= 1)
-			// 	angle*=-1;
 			console.log("VEC1 = "+vec1+", VEC2 = "+vec2+"\nANGLE = "+(angle/DEGREE_TO_RAD)+"\n cross ="+cross+" dot = "+dt_prod);
 			this.angles.push(angle);
     }
