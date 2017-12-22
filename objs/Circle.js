3e0f869d77b8e25030f256b6cbbe2d7101ece4e4
Almeida-Oco <intjalmeida@gmail.com>
2017-12-22 01:04:48 +0000

    Simple board done

--- objs/Circle.js
+++ objs/Circle.js
@@ -10,14 +10,12 @@ function Circle(scene, args) {
 	this.slices 	= args[0];
 	this.radius		= args[1];
 	this.angle 		= (2*Math.PI) / this.slices;
-
-	this.initBuffers();
 };
 
 Circle.prototype = Object.create(CGFobject.prototype);
 Circle.prototype.constructor = Circle;
 
-Circle.prototype.initBuffers = function() {
+Circle.prototype.initBuffers = function(afs, aft) {
 	this.vertices 	= [];
 	this.indices 		= [];
 	this.normals 		= [];
@@ -27,29 +25,27 @@ Circle.prototype.initBuffers = function() {
 	var theta = 0.0;
 	var center = this.index;
 
-	this.vertices.push(0); this.vertices.push(0); this.vertices.push(0);
-	this.texCoords.push(0.5, 0.5);
+	this.vertices.push(0, 0, 0);
+	this.texCoords.push(0.5 * afs, 0.5 * aft);
 	this.index++;
 
 	for(j = 0; j < this.slices; j++){
 		this.vertices.push(this.radius*Math.cos(theta)); this.vertices.push(this.radius*Math.sin(theta)); this.vertices.push(0);
-		this.texCoords.push((Math.cos(theta)+1)/2.0,((-Math.sin(theta)+1)/2.0));
+		this.texCoords.push((Math.cos(theta)+1) * afs/2.0,((-Math.sin(theta)+1) * aft/2.0));
 
  		this.index++;
 		theta += this.angle;
 
- 		this.vertices.push(this.radius*Math.cos(theta)); this.vertices.push(this.radius*Math.sin(theta)); this.vertices.push(0);
+ 		this.vertices.push(this.radius*Math.cos(theta), this.radius*Math.sin(theta), 0);
  		this.index++;
 
 
 
-		this.texCoords.push((Math.cos(theta)+1)/2.0,((-Math.sin(theta)+1)/2.0));
+		this.texCoords.push((Math.cos(theta)+1) * afs/2.0,((-Math.sin(theta)+1) * aft/2.0));
 
 
-		this.indices.push(center); this.indices.push(this.index-2); this.indices.push(this.index-1);
-		this.normals.push(0); this.normals.push(0); this.normals.push(1);
-		this.normals.push(0); this.normals.push(0); this.normals.push(1);
-		this.normals.push(0); this.normals.push(0); this.normals.push(1);
+		this.indices.push(center, this.index-2, this.index-1);
+		this.normals.push(0, 0, 1);	this.normals.push(0, 0, 1);	this.normals.push(0, 0, 1);
 
 	}
 
@@ -58,5 +54,8 @@ Circle.prototype.initBuffers = function() {
 }
 
 Circle.prototype.render = function (afs, aft) {
+	if (this.indices === undefined) {
+		this.initBuffers(afs, aft);
+	}
 	this.display();
 }
